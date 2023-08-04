72472603899c67e38d0405faef0e0ec527fad9aa
mnprasetya <mnprasetya@gmail.com>
2023-08-04 10:28:45 +0700

    livewire, folio, volt, breeze, spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,8 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
     /**
      * The attributes that are mass assignable.
      *
